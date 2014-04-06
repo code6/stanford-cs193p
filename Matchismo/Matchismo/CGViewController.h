04f6d3904e2df760e0d9dffc39417c7f9ddb3990
Jobert Sa <jobert@codespark.co>
2014-04-07 00:13:36 +0200

    Initial commit for Xcode workspace for CS193p course containing first project, a single view app for iPhone called “Matchismo”

---
+++ Matchismo/Matchismo/CGViewController.h
@@ -0,0 +1,13 @@
+//
+//  CGViewController.h
+//  Matchismo
+//
+//  Created by Jobert Sá on 4/7/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGViewController : UIViewController
+
+@end
---
+++ Matchismo/Matchismo/CGViewController.m
@@ -0,0 +1,29 @@
+//
+//  CGViewController.m
+//  Matchismo
+//
+//  Created by Jobert Sá on 4/7/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import "CGViewController.h"
+
+@interface CGViewController ()
+
+@end
+
+@implementation CGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
